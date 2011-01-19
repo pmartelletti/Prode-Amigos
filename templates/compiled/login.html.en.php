e09bc17ead49c9a5531db083cd1122bdd5cc9e91
Pablo Martelletti <pmartelletti@gmail.com>
2011-01-19 00:19:52 -0600

    Some more little changes

--- templates/compiled/login.html.en.php
+++ templates/compiled/login.html.en.php
@@ -1,17 +1,41 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
- <html>
-    <head>
-      <title>My Facebook Login Page</title>
-    </head>
-    <body>
-      <div id="fb-root"></div>
-      <script src="http://connect.facebook.net/en_US/all.js"></script>
-      <script>
-         FB.init({ 
-            appId:'124703570930710', cookie:true, 
-            status:true, xfbml:true 
-         });
-      </script>
-      <fb:login-button>Entrar con facebook</fb:login-button>
-    </body>
- </html>
+<script type="text/javascript">
+	$(document).ready(function(){
+		// proceso el login
+		$("#sendLogin").button().click(function(){
+			var datos = $("#loginForm").serializeArray();
+			$.post("index.php?action=login", datos, function(response){
+				var responseDiv = $("#response");
+				if(response.statusCode == "100"){
+					var respuesta = "<span class='ui-icon ui-icon-check'></span>" + response.statusMsg;
+					responseDiv.addClass("success").html(respuesta);
+					setTimeout('window.location = "index.php"',1000);
+					 
+				} else {
+					var respuesta = "<span class='ui-icon ui-icon-alert'></span>" + response.statusMsg;
+					responseDiv.addClass("error").html(respuesta);
+				}
+			}, "json");
+		})
+	})
+</script>
+
+<div class="span-3">&nbsp;</div>
+<div id="login" class="span-11 last">
+	<form id="loginForm" action="#">
+		<fieldset>
+			<h4>Login del Usuario</h4>
+			<p>
+				<label for="us_login">Usuario: </label>
+				<?php echo $this->elements['us_login']->toHtml();?>
+			</p>
+			
+			<p>
+				<label for="us_pass">Password: </label>
+				<?php echo $this->elements['us_pass']->toHtml();?>
+			</p>
+			<p><?php echo $this->elements['sendLogin']->toHtml();?></p>
+			
+			<div id="response"></div>
+		</fieldset>
+	</form>
+</div>
